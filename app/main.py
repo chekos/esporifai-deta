4a090ec620c5845d90bdc7bdc8681ef054cd1a63
sergio <sergio@cimarron.io>
2023-03-14 16:53:03 -0700

    add jinja template mvp

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from deta import Base
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+auth = Base("esporifai-auth")
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    context = {
+        "request": request,
+        "data": {"page_title": "Home", "page_content": "<h3>this works</h3>"},
+    }
+    return templates.TemplateResponse("page.html", context)
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("login.html", context)
