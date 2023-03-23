673f62b820fc9414bb807ab87c65fbc2786f2a29
chekos <sergio@cimarron.io>
2023-03-23 08:36:44 -0700

    add canvas

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,7 +1,14 @@
 async function takeScreenshot() {
+  window.scrollTo(0, 0);
   var trackDiv = document.querySelector("#track");
   var trackContainer = document.querySelector("#track-container");
-  html2canvas(trackDiv, { allowTaint: true }).then((canvas) => {
+  html2canvas(trackDiv, {
+    allowTaint: true,
+    windowWidth: trackDiv.scrollWidth,
+    windowHeight: trackDiv.windowHeight,
+    scale: window.devicePixelRatio,
+  }).then((canvas) => {
+    canvas.id = "track-container-canvas";
     trackContainer.appendChild(canvas);
   });
 }
