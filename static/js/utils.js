746f3e7361fe9de3659a032a4b527aa8f94e67bc
Sergio Sánchez Zavala <chekos.data@gmail.com>
2023-04-04 22:39:46 -0700

    Update utils.js

--- static/js/utils.js
+++ static/js/utils.js
@@ -12,3 +12,20 @@ async function takeScreenshot() {
     trackContainer.appendChild(canvas);
   });
 }
+
+function downloadDivAsImage() {
+  const divToCapture = document.getElementById('track');
+  const canvas = document.createElement('canvas');
+  canvas.width = divToCapture.offsetWidth;
+  canvas.height = divToCapture.offsetHeight;
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(divToCapture, 0, 0, canvas.width, canvas.height);
+  const dataURL = canvas.toDataURL('image/png');
+
+  const downloadLink = document.createElement('a');
+  downloadLink.href = dataURL;
+  downloadLink.download = 'screenshot.png';
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
