88fb812ae1823d2a3c4516b30d618bf2b6550645
Sergio Sánchez Zavala <28694175+chekos@users.noreply.github.com>
2023-10-30 21:24:24 -0700

    Update utils.js

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,14 +1,15 @@
-function captureAndDownload() {
+function captureAndAppend() {
     const trackDiv = document.getElementById("track");
 
     // Using dom-to-image to capture the element
     domtoimage.toPng(trackDiv)
         .then(function (dataUrl) {
-            // Create a temporary anchor element to download the image
-            const a = document.createElement("a");
-            a.href = dataUrl;
-            a.download = "track_image.png";
-            a.click();
+            // Create an img element
+            const img = new Image();
+            img.src = dataUrl;
+
+            // Append the img to a container, for example, to the body or another div
+            document.body.appendChild(img);
         })
         .catch(function (error) {
             console.error('Error capturing image:', error);
